b3cac9f1b063ee4ae4c64d198b6dfbc69fca4479
Sherif Elmetwally <sge5@njit.edu>
2020-09-11 21:54:35 -0400

    add chalk and nodemon

--- node-notes/app.js
+++ node-notes/app.js
@@ -1,6 +1,8 @@
 const validator = require('validator')
 const getNotes = require('./notes.js')
+const chalk = require('chalk')
 
 console.log(getNotes())
 console.log(validator.isEmail('sherif@gmail.com'))
+console.log(chalk.red('Success'));
 
